21fe3443d47ca1301d59e84f0502f234cecfb284
lishi-lin <675359876@qq.com>
2019-12-23 14:28:23 +0800

    头条初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(element)
 Vue.config.productionTip = false
 
 new Vue({
